e9a76cccd11a4a4b242b15ef4a75a7888e66f7d5
Shrestha Pal <2shrestha28@gmail.com>
2025-08-14 11:20:57 +0530

    Update updateCartValue.js

--- updateCartValue.js
+++ updateCartValue.js
@@ -1,5 +1,5 @@
 const cartValue = document.querySelector("#cartValue");
 
 export const updateCartValue = (cartProducts) => {
-  return (cartValue.innerHTML = ` <i class="fa-solid fa-cart-shopping"> ${cartProducts.length} </i>`);
+  return (cartValue.innerHTML = ` <i class="fa-solid fa-cart-shopping"> ${cartProducts.length} </i>`);   // it's basically array's length i.e arr.length
 };

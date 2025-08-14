7eeac68e36d96a470e1dec151bbdeaecbe6c3dd1
Shrestha Pal <2shrestha28@gmail.com>
2025-08-14 11:59:24 +0530

    Update updateCartProductTotal.js

--- updateCartProductTotal.js
+++ updateCartProductTotal.js
@@ -1,3 +1,21 @@
+// import { getCartProductFromLS } from "./getCartProducts";
+
+// export const updateCartProductTotal = () => {
+//   let productSubTotal = document.querySelector(".productSubTotal");
+//   let productFinalTotal = document.querySelector(".productFinalTotal");
+
+//   let localCartProducts = getCartProductFromLS();
+//   let initialValue = 0;
+//   let totalProductPrice = localCartProducts.reduce((accum, curElem) => {
+//     let productPrice = parseInt(curElem.price) || 0;
+//     return accum + productPrice;
+//   }, initialValue);
+//   //   console.log(totalProductPrice);
+
+//   productSubTotal.textContent = `₹${totalProductPrice}`;
+//   productFinalTotal.textContent = `₹${totalProductPrice + 50}`;
+// };
+
 import { getCartProductFromLS } from "./getCartProducts";
 
 export const updateCartProductTotal = () => {
@@ -6,12 +24,19 @@ export const updateCartProductTotal = () => {
 
   let localCartProducts = getCartProductFromLS();
   let initialValue = 0;
+
   let totalProductPrice = localCartProducts.reduce((accum, curElem) => {
     let productPrice = parseInt(curElem.price) || 0;
     return accum + productPrice;
   }, initialValue);
-  //   console.log(totalProductPrice);
 
+  // Update subtotal
   productSubTotal.textContent = `₹${totalProductPrice}`;
-  productFinalTotal.textContent = `₹${totalProductPrice + 50}`;
+
+  // Add ₹50 only if there is at least one product
+  if (localCartProducts.length > 0) {
+    productFinalTotal.textContent = `₹${totalProductPrice + 50}`;
+  } else {
+    productFinalTotal.textContent = `₹0`;
+  }
 };

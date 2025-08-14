9ea187fd2bbccbfe50d827f17eaeb088b6f6c20c
Shrestha Pal <2shrestha28@gmail.com>
2025-08-14 09:40:45 +0530

    Update homeQuantityToggle.js

--- homeQuantityToggle.js
+++ homeQuantityToggle.js
@@ -1,10 +1,12 @@
 export const homeQuantityToggle = (event, id, stock) => {
-    const currentCardElement = document.querySelector(`#card${id}`);
+    // Uses document.querySelector(#card${id}) so only the clicked card changes.
+    const currentCardElement = document.querySelector(`#card${id}`);  
     //   console.log(currentCardElement);
   
     const productQuantity = currentCardElement.querySelector(".productQuantity");
     //   console.log(productQuantity);
-  
+
+    // Read the current quantity from a hidden data-quantity attribute (default is 1). [getAttribute will be shown in the console panel while inspecting]
     let quantity = parseInt(productQuantity.getAttribute("data-quantity")) || 1;
   
     if (event.target.className === "cartIncrement") {
@@ -20,9 +22,13 @@ export const homeQuantityToggle = (event, id, stock) => {
         quantity -= 1;
       }
     }
-    
+
+
+    // updating the visible quantity (innertext)
     productQuantity.innerText = quantity;
     console.log(quantity);
+
+    // updating the hidden quantity so the future clicks know the latest no.
     productQuantity.setAttribute("data-quantity", quantity.toString());
     return quantity;
   };
